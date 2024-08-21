39038a0e2d1c43f851c9ad95be9ff7cb67d4af8a
wucc <1528956427@qq.com>
2024-08-22 00:04:06 +0800

    cc

--- demo-mq-kafka/src/test/java/com/xkcoding/mq/kafka/SpringBootDemoMqKafkaApplicationTests.java
+++ demo-mq-kafka/src/test/java/com/xkcoding/mq/kafka/SpringBootDemoMqKafkaApplicationTests.java
@@ -8,19 +8,43 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootDemoMqKafkaApplicationTests {
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+  @Autowired
+  private KafkaTemplate<String, String> kafkaTemplate;
 
-    /**
-     * 测试发送消息
-     */
-    @Test
-    public void testSend() {
-        kafkaTemplate.send(KafkaConsts.TOPIC_TEST, "hello,kafka...");
+  /**
+   * 测试发送消息
+   */
+  @Test
+  public void testSend() {
+    kafkaTemplate.send(KafkaConsts.TOPIC_TEST, "hello,kafka...");
+  }
+
+  @Test
+  public void sendMutily() {
+
+    AtomicInteger count = new AtomicInteger();
+    ExecutorService executorService = Executors.newFixedThreadPool(1000);
+    for (int j = 0; j < 1000; j++) {
+      executorService.submit(() -> {
+        for (int i = 0; i < 100000; i++) {
+          kafkaTemplate.send(KafkaConsts.TOPIC_TEST, "hello,kafka..." + i);
+          count.getAndIncrement();
+        }
+      });
     }
+    while (!executorService.isTerminated()) {
+      //System.out.println("hahaa");
+    }
+
+  }
+
 
 }
 
